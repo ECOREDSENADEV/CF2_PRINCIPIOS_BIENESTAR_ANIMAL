aa2cfef54b999cb69c1e7e02886367a40419e9d1
devcontenidosinteractivos <98177573+AndresDiazPinto@users.noreply.github.com>
2025-12-28 14:22:46 -0500

    Initial commit
    Se realiza banner, intro, sintesis, globla, img

--- src/config/global.js
+++ src/config/global.js
@@ -1,17 +1,23 @@
 export default {
   global: {
-    Name: 'Nombre del recurso educativo',
-    Description: 'Descripción del RED',
-    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.svg'),
+    Name:
+      'Gestión del bienestar animal en la tenencia responsable de animales de compañía',
+    Description:
+      'El componente formativo explica la tenencia responsable de mascotas, incluyendo deberes legales y éticos para asegurar su bienestar. Aborda alimentación, salud, vacunación, desparasitación, bioseguridad, identificación y registro. Destaca el control de zoonosis, la salud pública y el manejo humanitario. Resalta leyes colombianas que protegen a los animales como seres sintientes y obligan a evitar el sufrimiento.',
+    imagenBannerPrincipal: require('@/assets/curso/portada/banner-principal.png'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
       {
         clases: ['banner-principal-decorativo-1', 'd-none', 'd-lg-block'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-1.png'),
       },
       {
         clases: ['banner-principal-decorativo-2'],
-        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.svg'),
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-2.png'),
+      },
+      {
+        clases: ['banner-principal-decorativo-3'],
+        imagen: require('@/assets/curso/portada/banner-principal-decorativo-3.png'),
       },
     ],
   },
@@ -31,28 +37,106 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Tenencia responsable',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Concepto de tenencia responsable',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Mecanismos de identificación',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Registro de la mascota',
+            hash: 't_1_3',
           },
         ],
       },
-
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Salud animal',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Medicina preventiva',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Vacunación',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Desparasitación',
+            hash: 't_2_3',
+          },
+          {
+            numero: '2.4',
+            titulo: 'Signos tempranos de enfermedad',
+            hash: 't_2_4',
+          },
+          {
+            numero: '2.5',
+            titulo: 'Medidas de bioseguridad',
+            hash: 't_2_5',
+          },
+          {
+            numero: '2.6',
+            titulo: 'Salud pública',
+            hash: 't_2_6',
+          },
+          {
+            numero: '2.7',
+            titulo: 'Zoonosis asociadas a mascotas',
+            hash: 't_2_7',
+          },
+          {
+            numero: '2.8',
+            titulo: 'Enfermedades de control oficial',
+            hash: 't_2_8',
+          },
+          {
+            numero: '2.9',
+            titulo: 'Normativa sanitaria aplicable en Colombia',
+            hash: 't_2_9',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Manejo humanitario',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Cuidados paliativos',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Enfermedades degenerativas',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Condición crónica degenerativa',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.4',
+            titulo: 'Enfoque legal y responsabilidad del tutor',
+            hash: 't_3_4',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -102,22 +186,128 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: 'Sitio web',
-      link: '',
+      tema: 'Tenencia responsable',
+      referencia:
+        'Ministerio de Salud y Protección Social. (2017). <em>Lineamientos para la política de tenencia responsable de animales de compañía y de producción</em>.',
+      tipo: 'Manual / guía',
+      link:
+        'https://www.minsalud.gov.co/sites/rid/Lists/BibliotecaDigital/RIDE/VS/PP/SA/lineamientos-tenencia-responsables-acy.pdf',
+    },
+    {
+      tema: 'Salud animal',
+      referencia:
+        'Moreno–Velásquez, J., Sánchez, P., Zurita, H., Santacruz, D., & Gonzales, R. (2020). <em>Aves de compañía: aproximación al bienestar animal en la tenencia responsable</em>. Instituto Distrital de Protección y Bienestar Animal.',
+      tipo: 'Documento oficial / normativa',
+      link:
+        'https://www.animalesbog.gov.co/documentos/35_Aves_tenenciaresponsable2020.pdf',
+    },
+    {
+      tema: 'Salud animal',
+      referencia:
+        'Ibáñez, M. J., & Perera, M. (2012). <em>Conducta y manejo avanzado en perros problemáticos: Sigue las huellas del camino correcto</em> (1.ª ed.). Ediciones Díaz de Santos.',
+      tipo: 'Libro técnico',
+      link: 'https://www.editdiazdesantos.com/wwwdat/pdf/9788499693880.pdf',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Bienestar animal',
+      significado:
+        'Estado en el que un animal vive libre de hambre, dolor y miedo, y puede expresar sus comportamientos naturales.',
+    },
+    {
+      termino: 'Bioseguridad',
+      significado:
+        'Conjunto de medidas destinadas a evitar la entrada y propagación de enfermedades en el hogar, incluyendo higiene, cuarentenas y control de residuos.',
+    },
+    {
+      termino: 'Eutanasia humanitaria',
+      significado:
+        'Procedimiento ético realizado por un profesional para terminar el sufrimiento de un animal con enfermedad irreversible, garantizando ausencia de dolor y estrés.',
+    },
+    {
+      termino: 'Identificación',
+      significado:
+        'Método que permite reconocer y vincular a una mascota con su tutor, mediante microchips, collares, placas o códigos QR.',
+    },
+    {
+      termino: 'Medicina preventiva',
+      significado:
+        'Conjunto de acciones orientadas a evitar enfermedades, como vacunación, desparasitación, controles veterinarios periódicos y alimentación adecuada.',
+    },
+    {
+      termino: 'Normativa',
+      significado:
+        'Conjunto de leyes y disposiciones que regulan la protección, sanidad y bienestar de los animales en Colombia.',
+    },
+    {
+      termino: 'Registro',
+      significado:
+        'Inscripción oficial de una mascota en bases de datos para control sanitario, identificación y localización en caso de pérdida.',
+    },
+    {
+      termino: 'Ser sintiente',
+      significado:
+        'Reconocimiento legal y ético de que los animales pueden sentir dolor, placer, miedo y otras emociones.',
+    },
+    {
+      termino: 'Tenencia responsable',
+      significado:
+        'Compromiso permanente del tutor para garantizar el bienestar físico, emocional y sanitario de su mascota.',
+    },
+    {
+      termino: 'Zoonosis',
+      significado:
+        'Enfermedades que pueden transmitirse de los animales a los seres humanos, como la rabia o la toxoplasmosis.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Congreso de la República de Colombia. (2016). <em>Ley 1774 de 2016 por medio de la cual se modifican el Código Civil, la Ley 84 de 1989 y el Código Penal, y se dictan otras disposiciones sobre protección animal</em>. Diario Oficial No. 49.747.',
+      link:
+        'https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=68135',
+    },
+    {
+      referencia:
+        'Congreso de la República de Colombia. (1989). <em>Ley 84 de 1989, Estatuto Nacional de Protección de los Animales</em>. Diario Oficial No. 39.120.',
+      link:
+        'https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=8242',
+    },
+    {
+      referencia:
+        'Ministerio de Agricultura y Desarrollo Rural. (2017). <em>Política pública de protección y bienestar animal</em>. Gobierno de Colombia.',
+      link:
+        'https://www.minambiente.gov.co/wp-content/uploads/2025/05/VF_PoliticaNacionalProteccionBienestarAnimal_7_5_25.pdf',
+    },
+    {
+      referencia:
+        'Ministerio de Salud y Protección Social. (2017). <em>Lineamientos para la tenencia responsable de animales de compañía</em>. Gobierno de Colombia.',
+      link:
+        'https://www.minsalud.gov.co/sites/rid/Lists/BibliotecaDigital/RIDE/VS/PP/SA/lineamientos-tenencia-responsables-acy.pdf',
+    },
+    {
+      referencia:
+        'Organización Mundial de Sanidad Animal – OMSA. (2019). <em>Código sanitario para los animales terrestres</em>. OMSA.',
+      link: 'https://www.woah.org/es/que-hacemos/normas/codigos-y-manuales/',
+    },
+    {
+      referencia:
+        'República de Colombia. (2025). <em>Ley 2455 de 18 de abril de 2025 por la cual se fortalece la lucha contra el maltrato animal y se actualiza el Estatuto Nacional de Protección de los Animales</em>. Diario Oficial No. 53.162.',
+      link:
+        'https://www.funcionpublica.gov.co/eva/gestornormativo/norma.php?i=259656',
+    },
+    {
+      referencia:
+        'Serpell, J. (Ed.). (2022). <em>The domestic dog: Its evolution, behavior and interactions with people</em> (2nd ed.). Cambridge University Press.',
+      link:
+        'https://books.google.es/books?id=I8HU_3ycrrEC&printsec=frontcover#v=onepage&q&f=false',
+    },
+    {
+      referencia:
+        'World Organisation for Animal Health – WOAH. (2021). <em>Animal welfare and five freedoms</em>. WOAH.',
+      link:
+        'https://www.woah.org/en/what-we-do/animal-health-and-welfare/animal-welfare/',
     },
   ],
   creditos: [
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -7,6 +7,31 @@
         span
           i.fas.fa-info
       h1 Introducción
+    
+    .row.mb-5.align-items-center
+      .col-lg-3.order-lg-2.mb-3.mb-lg-0
+        figure
+          img(src="@/assets/curso/intro/1.png", data-aos="zoom-in")
+      .col-lg-9.order-lg-1
+        p(data-aos="fade-left") La tenencia responsable de animales de compañía es un compromiso ético, legal y social que implica mucho más que brindar alimento y refugio. Significa garantizar el bienestar integral de las mascotas, entendidas como seres sintientes, capaces de experimentar emociones y dolor. Este enfoque promueve una relación respetuosa y consciente entre humanos y animales.
+        .bg-color-7(data-aos="fade-up")
+          .row.justify-content-center.align-items-center
+            .col-lg-auto.mb-3.mb-lg-0
+              figure
+                img(src='@/assets/curso/intro/2.svg', alt='')
+            .col-lg
+              .p-4
+                p.mb-0(data-aos="fade-up") En Colombia, la normativa vigente establece obligaciones claras para los tutores, incluyendo la identificación, el registro, la atención veterinaria, la prevención de enfermedades y la protección contra el maltrato. Estas medidas no solo benefician a los animales, sino que también protegen la salud pública y el medio ambiente, bajo el principio de Una Sola Salud.
+    
+    .tarjeta.bg-color-10.mb-5
+      .px-4.px-md-5.py-4
+        .row.align-items-center
+          .col-lg-auto
+            img(src="@/assets/curso/intro/3.png", style="max-width: 90px").mx-auto
+          .col-lg.text-white
+            p.mb-0 El presente documento desarrolla los aspectos fundamentales de la tenencia responsable, la salud animal y el manejo humanitario. Además, destaca la importancia de la medicina preventiva, el control de zoonosis, las obligaciones legales del tutor y la necesidad de garantizar una vida digna para todas las especies domésticas, tanto convencionales como no convencionales.
+    
+       
 
 </template>
 
